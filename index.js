49e0b361d730d389483d8427a1694d5c4262cae0
francelaquino <francel_aquino@yahoo.com>
2019-04-06 21:00:31 +0300

    1

--- index.js
+++ index.js
@@ -23,28 +23,34 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.get('/data/login/:mobileno',(req,res)=>{
-    
-    const queryString=`select  * from family where id=parentid and mobile='${req.params.mobileno}'`;
 
-    pool.query(queryString,(err,rows,fields)=>{
-        
-        if(err){
+app.get('/data/login/:mobileno', (req, res) => {
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
             res.status(500).json({
-                message:'Something went wrong!'
+                message: 'Something went wrong!'
             })
-            
-        }else{
-            res.status(200).json({
-                results:rows,
-            })
-        
+        } else {
+
+            const queryString = `select  * from family where id=parentid and mobile='${req.params.mobileno}'`;
+
+            pool.query(queryString, (err, rows, fields) => {
+
+                if (err) {
+                    res.status(500).json({
+                        message: 'Something went wrong!'
+                    })
+                    conn.release();
+                } else {
+                    res.status(200).json({
+                        results: rows,
+                    })
+                    conn.release();
+                }
+            });
         }
     });
 
-    pool.end();
-
-   
 });
 
 app.get('/data/getfamily/:id/:gid',(req,res)=>{
@@ -74,134 +80,149 @@ app.get('/data/getfamily/:id/:gid',(req,res)=>{
     });
 });
 
+app.post('/data/savefamilymember', (req, res) => {
 
-
-
-app.post('/data/savefamilymember',(req,res)=>{
-    
-  
-    const queryString=`select  * from family where parentid=${req.body.id} and gid='${req.body.gid}'`;
-
-    pool.query(queryString,(err,rows,fields)=>{
-       
-        if(err){
-           
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
             res.status(500).json({
-                message:'Something went wrong!'
+                message: 'Something went wrong!'
             })
-            
-            return;
-            
-        }else{
-            if(rows.length>0){
-                let secondname=rows[0].secondname;
-                let thirdname=rows[0].thirdname;
-                let fourthname=rows[0].fourthname;
-                let fifthname=rows[0].fifthname;
-                let sixname=rows[0].sixname;
+        } else {
 
-                const queryString=`insert into family(parentid,gid,firstname,secondname,thirdname,fourthname,fifthname,sixname,gender,
+            const queryString = `select  * from family where parentid=${req.body.id} and gid='${req.body.gid}'`;
+
+            pool.query(queryString, (err, rows, fields) => {
+
+                if (err) {
+
+                    return res.status(500).json({
+                        message: 'Something went wrong!'
+                    })
+
+
+                } else {
+                    if (rows.length > 0) {
+                        let secondname = rows[0].secondname;
+                        let thirdname = rows[0].thirdname;
+                        let fourthname = rows[0].fourthname;
+                        let fifthname = rows[0].fifthname;
+                        let sixname = rows[0].sixname;
+
+                        const queryString = `insert into family(parentid,gid,firstname,secondname,thirdname,fourthname,fifthname,sixname,gender,
                     birthdate,email,maritalstatus,jobstatus,mobile,address,dateupdated)
                     values(${req.body.id},'${req.body.gid}','${req.body.firstnameMember}','${secondname}','${thirdname}',
                     '${fourthname}','${fifthname}','${sixname}','${req.body.genderMember}','${req.body.birthdateMember}','${req.body.emailMember}',
                     '${req.body.maritalstatusMember}','${req.body.jobstatusMember}','${req.body.mobilenoMember}','${req.body.addressMember}',curdate())`;
 
-                    pool.query(queryString,(err)=>{
-                        if(err){
-                            res.status(500).json({
-                                message:'Something went wrong!'
-                            })
-                            
-                        }else{
-                            res.status(200).json({
-                                message:'',
-                            })
-                         
-                        }
-                    });
+                        pool.query(queryString, (err) => {
+                            if (err) {
+                                res.status(500).json({
+                                    message: 'Something went wrong!'
+                                })
 
-            }
-        
+                            } else {
+                                res.status(200).json({
+                                    message: '',
+                                })
+
+                            }
+                        });
+
+                    }
+                    conn.release();
+
+                }
+
+            });
         }
-
     });
 
-    pool.end();
-
-   
 });
-
 app.post('/data/register', (req, res) => {
 
     let gid = uuidv4();
 
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
+            res.status(500).json({
+                message: 'Something went wrong!'
+            })
+        } else {
 
-    const queryString = `insert into family(gid,firstname,secondname,thirdname,fourthname,fifthname,sixname,gender,
+            const queryString = `insert into family(gid,firstname,secondname,thirdname,fourthname,fifthname,sixname,gender,
                     birthdate,email,maritalstatus,jobstatus,mobile,address,dateupdated)
                     values('${gid}','${req.body.firstname}','${req.body.secondname}','${req.body.thirdname}',
                     '${req.body.fourthname}','${req.body.fifthname}','${req.body.sixname}','${req.body.gender}','${req.body.birthdate}','${req.body.email}',
                     '${req.body.maritalstatus}','${req.body.jobstatus}','${req.body.mobileno}','${req.body.address}',curdate())`;
 
-    pool.query(queryString, (err, result) => {
-        if (err) {
-            res.status(500).json({
-                message: 'Something went wrong!'
-            })
-
-        } else {
-            let Id=result.insertId;
-            const queryString = `update family set parentid=${Id} where id=${Id}`;
-
-            pool.query(queryString, (err) => {
+            pool.query(queryString, (err, result) => {
                 if (err) {
                     res.status(500).json({
                         message: 'Something went wrong!'
                     })
 
                 } else {
+                    let Id = result.insertId;
+                    const queryString = `update family set parentid=${Id} where id=${Id}`;
+
+                    pool.query(queryString, (err) => {
+                        if (err) {
+                            res.status(500).json({
+                                message: 'Something went wrong!'
+                            })
+
+                        } else {
 
 
-                    res.status(200).json({
-                        results: '?id='+Id+'&gid='+gid,
-                    })
+                            res.status(200).json({
+                                results: '?id=' + Id + '&gid=' + gid,
+                            })
+
+                        }
+                    });
+
+                    conn.release();
+
+
 
                 }
             });
-
         }
     });
-
-    pool.end();
-
 
 });
 
+app.put('/data/updatefamilymember', (req, res) => {
 
-app.put('/data/updatefamilymember',(req,res)=>{
-    
-
-    const queryString=`update family set firstname='${req.body.firstname}',gender='${req.body.gender}',
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
+            res.status(500).json({
+                message: 'Something went wrong!'
+            })
+        } else {
+            const queryString = `update family set firstname='${req.body.firstname}',gender='${req.body.gender}',
     birthdate='${req.body.birthdate}',email='${req.body.email}',maritalstatus='${req.body.maritalstatus}',jobstatus='${req.body.jobstatus}',
     mobile='${req.body.mobileno}',address='${req.body.address}',dateupdated=curdate() where id=${req.body.mid} `;
 
-    pool.query(queryString,(err)=>{
-        
-        if(err){
-            res.status(500).json({
-                message:'Something went wrong!'
-            })
-            
-        }else{
-            res.status(200).json({
-                message:'',
-            })
-         
+            pool.query(queryString, (err) => {
+
+                if (err) {
+                    res.status(500).json({
+                        message: 'Something went wrong!'
+                    })
+
+                } else {
+                    res.status(200).json({
+                        message: '',
+                    })
+
+                }
+                conn.release();
+            });
         }
     });
 
-    pool.end();
 
-   
 });
 
 
@@ -209,6 +230,12 @@ app.put('/data/updatefamilymember',(req,res)=>{
 app.put('/data/updatemain',(req,res)=>{
     
 
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
+            res.status(500).json({
+                message: 'Something went wrong!'
+            })
+        } else {
     const queryString=`update family set firstname='${req.body.firstname}',secondname='${req.body.secondname}',thirdname='${req.body.thirdname}',
     fourthname='${req.body.fourthname}',fifthname='${req.body.fifthname}',sixname='${req.body.sixname}',gender='${req.body.gender}',
     birthdate='${req.body.birthdate}',email='${req.body.email}',maritalstatus='${req.body.maritalstatus}',jobstatus='${req.body.jobstatus}',
@@ -227,9 +254,13 @@ app.put('/data/updatemain',(req,res)=>{
             })
          
         }
+
+        conn.release();
     });
 
-    pool.end();
+}
+
+    });
 
    
 });
@@ -269,6 +300,13 @@ app.get('/data/getrecord/:id/:gid',(req,res)=>{
 
 app.get('/data/getfamilyrecord/:mid/',(req,res)=>{
     
+    pool.getConnection((errorPool, conn) => {
+        if (errorPool) {
+            res.status(500).json({
+                message: 'Something went wrong!'
+            })
+        } else {
+
     const queryString=`select  * from family where id=${req.params.mid}`;
 
     pool.query(queryString,(err,rows,fields)=>{
@@ -284,9 +322,13 @@ app.get('/data/getfamilyrecord/:mid/',(req,res)=>{
             })
          
         }
+        conn.release();
     });
 
-    pool.end();
+         
+}
+});
+
 
    
 });
